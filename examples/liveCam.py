6573ef2d9d4b4f3763073b20fa10e4ad5bca2815
Adrian Lubitz <adrian.lubitz@dfki.de>
2021-03-29 13:10:54 +0200

    def for demo

---
+++ examples/liveCam.py
@@ -0,0 +1,13 @@
+'''
+This is a live Demo using the webcam
+'''
+
+# System imports
+
+# 3rd party imports
+# Here I want to import from the lib.
+
+# local imports
+
+# end file header
+__author__      = 'Adrian Lubitz'
