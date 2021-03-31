eeda815a6c368bb4544f7328314e2d0a5c59e188
Adrian Lubitz <adrianlubitz@gmail.com>
2021-03-31 12:49:37 +0200

    started liveCam

--- examples/liveCam.py
+++ examples/liveCam.py
@@ -5,9 +5,20 @@ This is a live Demo using the webcam
 # System imports
 
 # 3rd party imports
-# Here I want to import from the lib.
+from vvadlrs3 import pretrained_models
 
 # local imports
 
 # end file header
-__author__      = 'Adrian Lubitz'
+__author__ = 'Adrian Lubitz'
+
+# Sliding window approach
+
+# load pretrained model
+model = pretrained_models.getFaceImageModel()
+
+# create a sample
+# create a webcambuffer
+# create_sample_from_buffer
+# infer if sample is valid
+# annotate and visualize output image(first of sample data) - do I need to save the boundingbox as well in the sample?
