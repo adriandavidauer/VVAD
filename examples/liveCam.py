af5d730665dc74e2faee360a8bfff9671d5d76d7
Adrian Lubitz <adrianlubitz@gmail.com>
2021-03-31 13:48:30 +0200

    some progress

--- examples/liveCam.py
+++ examples/liveCam.py
@@ -6,6 +6,9 @@ This is a live Demo using the webcam
 
 # 3rd party imports
 from vvadlrs3 import pretrained_models
+from vvadlrs3 import sample
+import cv2
+
 
 # local imports
 
@@ -17,8 +20,19 @@ __author__ = 'Adrian Lubitz'
 # load pretrained model
 model = pretrained_models.getFaceImageModel()
 
-# create a sample
+
 # create a webcambuffer
+cap = cv2.VideoCapture(0)
+
+
+# create a sample
 # create_sample_from_buffer
 # infer if sample is valid
 # annotate and visualize output image(first of sample data) - do I need to save the boundingbox as well in the sample?
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
