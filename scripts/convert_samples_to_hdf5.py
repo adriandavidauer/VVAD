3791571568431fa86f5e04d6cd8a04cea4ac6e00
Adrian Lubitz <adrianlubitz@gmail.com>
2021-05-13 09:41:21 +0200

    adjusted to handle different datasets automatically

--- scripts/convert_samples_to_hdf5.py
+++ scripts/convert_samples_to_hdf5.py
@@ -31,28 +31,27 @@ if __name__ == '__main__':
     input_folder = Path(args.input_folder)
     test_set = input_folder / 'testSet'
 
-    x_chunk_shape = (args.chunk_resolution, 38, 200, 200, 3)
-    y_chunk_shape = (args.chunk_resolution,)
-
     train_samples = list(input_folder.glob('*/*.pickle'))
     test_samples = list(test_set.glob('*/*.pickle'))
 
-    print(f'len(train_samples): {len(train_samples)}')
-    print(f'len(test_samples): {len(test_samples)}')
+    # Get shape of a sample
+    _sample = FeatureizedSample()
+    _sample.load(train_samples[0])
+    sample_shape = _sample.getData().shape
+
+    x_chunk_shape = (args.chunk_resolution, *sample_shape)
+    y_chunk_shape = (args.chunk_resolution,)
 
     out_h5 = h5py.File(args.output_file, "w")
-    x_train_ds = out_h5.create_dataset(shape=(len(train_samples), 38, 200, 200, 3), dtype=np.uint8,
+    x_train_ds = out_h5.create_dataset(shape=(len(train_samples), *sample_shape), dtype=np.uint8,
                                        name="x_train", chunks=x_chunk_shape, compression="gzip", compression_opts=9)
     y_train_ds = out_h5.create_dataset(shape=(
         len(train_samples),), name="y_train", dtype=np.uint8, chunks=y_chunk_shape, compression="gzip", compression_opts=9)
 
-    x_test_ds = out_h5.create_dataset(shape=(len(test_samples), 38, 200, 200, 3), dtype=np.uint8,
+    x_test_ds = out_h5.create_dataset(shape=(len(test_samples), *sample_shape), dtype=np.uint8,
                                       name="x_test", chunks=x_chunk_shape, compression="gzip", compression_opts=9)
     y_test_ds = out_h5.create_dataset(shape=(
         len(test_samples),), name="y_test", dtype=np.uint8, chunks=y_chunk_shape, compression="gzip", compression_opts=9)
-
-    # train_count = 0
-    # test_count = 0
 
     # Loop for the training samples
     print('converting training samples')
@@ -61,15 +60,9 @@ if __name__ == '__main__':
         s.load(sample)
         data = s.getData()  # The np.array for that sample
         label = s.getLabel()  # The label as int for that sample
-        # TODO: @matias: add hdf5 magic here
-
-        # x_train_ds.resize(train_count + 1, axis=0)
-        # y_train_ds.resize(train_count + 1, axis=0)
 
         x_train_ds[i] = data
         y_train_ds[i] = label
-
-        # train_count += 1
 
     # Loop for the test samples
     print('converting test samples')
@@ -78,14 +71,8 @@ if __name__ == '__main__':
         s.load(sample)
         data = s.getData()  # The np.array for that sample
         label = s.getLabel()  # The label as int for that sample
-        # TODO: @matias: add hdf5 magic here
-
-        # x_test_ds.resize(test_count + 1, axis=0)
-        # y_test_ds.resize(test_count + 1, axis=0)
 
         x_test_ds[i] = data
         y_test_ds[i] = label
-
-        # test_count += 1
 
     out_h5.close()
